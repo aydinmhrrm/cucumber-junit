39e7c6144ecb4bf2a61a7ac65846538ed183ca0f
aydinmhrrm <aydin.mhrrm16@gmail.com>
2022-03-18 11:21:07 -0400

    Update

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,14 +1,34 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage=new GoogleSearchPage();
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
+    }
+
+    @Then("user sees apple in the title")
+    public void user_sees_apple_in_the_title() {
+        String expectedTitle="apple - Google Search";
+        String actualTitle=Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle,actualTitle);
+       // Assert.assertTrue(actualTitle,equals(expectedTitle));
+    }
+
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
-        System.out.println("User is on google search page");
+        //System.out.println("User is on google search page");
 
         Driver.getDriver().get("https://www.google.com");
     }
@@ -23,5 +43,7 @@ public class GoogleStepDefinitions {
 
         Driver.closeDriver();
     }
+
+
 
 }
