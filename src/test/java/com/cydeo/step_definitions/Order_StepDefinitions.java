10b9b27f94f9213c1ae2dd85b57434c69be7f1f9
aydinmhrrm <aydin.mhrrm16@gmail.com>
2022-03-22 12:38:14 -0400

    Update

--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -2,23 +2,24 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.pages.BasePage;
 import com.cydeo.pages.OrderPage;
+import com.cydeo.pages.ViewAllOrderPage;
 import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-
-import java.util.List;
 
 public class Order_StepDefinitions {
     WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
     BasePage basePage = new BasePage();
     OrderPage orderPage = new OrderPage();
+    ViewAllOrderPage viewAllOrderPage = new ViewAllOrderPage();
 
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
@@ -65,7 +66,7 @@ public class Order_StepDefinitions {
 
     @When("user enters street {string}")
     public void user_enters_street(String string) {
-        orderPage.inputState.sendKeys(string);
+        orderPage.inputStreet.sendKeys(string);
     }
 
     @When("user enters city {string}")
@@ -85,32 +86,30 @@ public class Order_StepDefinitions {
 
     @When("user selects credit card type {string}")
     public void user_selects_credit_card_type(String expectedCardType) {
-        List<WebElement> cardTypes=orderPage.cardType;
-        for (WebElement each: cardTypes){
-            if (each.getAttribute("value").equalsIgnoreCase(expectedCardType)){
-                each.click();
-            }
-        }
+        //This line will loop through the list and decide which radio button to click
+        BrowserUtils.clickRadioButton(orderPage.cardType, expectedCardType);
     }
 
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-
+        orderPage.cardNoInput.sendKeys(string);
     }
 
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-
+        orderPage.cardExpInput.sendKeys(string);
     }
 
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-
+        orderPage.processOrderButton.click();
     }
 
     @Then("user should see {string} in first row of the web table")
-    public void user_should_see_in_first_row_of_the_web_table(String string) {
+    public void user_should_see_in_first_row_of_the_web_table(String expectedName) {
+        String actualName = viewAllOrderPage.newCustomerCell.getText();
 
+        Assert.assertEquals(expectedName, actualName);
     }
 
 }
